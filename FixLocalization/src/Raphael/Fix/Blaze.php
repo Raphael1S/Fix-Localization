aff1614535012c77d72bc24664052d16e2ce4518
Raphael S <131126712+Raphael1S@users.noreply.github.com>
2023-04-18 11:28:13 -0300

    Create Blaze.php

---
+++ FixLocalization/src/Raphael/Fix/Blaze.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Raphael\Fix;
+
+use pocketmine\plugin\PluginBase;
+
+class Blaze extends PluginBase {
+
+    public function onEnable() {
+        $this->atualizarArquivoPocketmineYml();
+    }
+
+    public function atualizarArquivoPocketmineYml() {
+        $filePath = "/home/container/pocketmine.yml";
+
+        if (file_exists($filePath)) {
+            $contents = file_get_contents($filePath);
+            $contents = preg_replace('/max-mtu-size:\s*\d+/', 'max-mtu-size: 1000', $contents);
+            file_put_contents($filePath, $contents);
+            $this->getLogger()->warning("O arquivo pocketmine.yml foi atualizado com sucesso.");
+        } else {
+            $this->getLogger()->warning("O arquivo pocketmine.yml não foi encontrado.");
+        }
+    }
+
+}
