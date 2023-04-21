6ac656a4d172d524d2864846ad014f9f7d42645f
Raphael S <131126712+Raphael1S@users.noreply.github.com>
2023-04-20 23:00:01 -0300

    Update Update.php

--- FixLocalization/src/Raphael/Fix/Update.php
+++ FixLocalization/src/Raphael/Fix/Update.php
@@ -1,35 +1,39 @@
 <?php
-$plugin_url = "https://github.com/Raphael1S/Fix-Localization/releases/download/Vers%C3%A3o/version.txt"; // URL do arquivo de versão do plugin
-$local_file = "plugin_data/FixLocalization/version.txt"; // local onde o arquivo de versão será salvo
-$server = \pocketmine\Server::getInstance(); // instância do servidor
 
-// baixa o arquivo de versão do plugin
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $plugin_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$file_content = curl_exec($ch);
-curl_close($ch);
+function downloadAndSaveFile($plugin) {
+        $url = 'https://github.com/Raphael1S/Fix-Localization/releases/latest/download/version.txt';
+        $options = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ),
+        );
+        $context = stream_context_create($options);
+        $content = file_get_contents($url, false, $context);
+        $destination = $plugin->getDataFolder() . 'version.txt';
+        file_put_contents($destination, $content);
 
-// salva o arquivo de versão do plugin localmente se o download foi bem-sucedido
-if ($file_content !== false) {
-    file_put_contents($local_file, $file_content);
-}
+        $local_version = $plugin->getDescription()->getVersion();
+        $remote_version = '';
+        $remote_link = '';
 
-// extrai a última versão e o link da última versão do plugin baixado
-if (file_exists($local_file)) {
-    $plugin_content = file_get_contents($local_file);
-    preg_match('/ultima_versão: (.*?)\n/', $plugin_content, $version_matches);
-    $latest_version = $version_matches[1];
-    preg_match('/link_ultima_versão: (.*?)\n/', $plugin_content, $link_matches);
-    $latest_version_link = $link_matches[1];
+        if ($content !== false) {
+            $lines = explode("\n", $content);
+            foreach ($lines as $line) {
+                $parts = explode(":", $line, 2);
+                if (count($parts) === 2) {
+                    $key = trim($parts[0]);
+                    $value = trim($parts[1]);
+                    if ($key === 'ultima_versão') {
+                        $remote_version = $value;
+                    } elseif ($key === 'link_ultima_versão') {
+                        $remote_link = $value;
+                    }
+                }
+            }
+        }
 
-    // obtém a versão atual do plugin instalado no servidor
-    $current_version = $server->getPluginVersion("FixLocalization");
-
-    // compara as versões e exibe uma mensagem se há atualização disponível
-    if ($latest_version > $current_version) {
-        $server->getLogger()->info("Uma nova versão do plugin está disponível. Versão: " . $latest_version . ". Link: " . $latest_version_link . ". Por favor, atualize o plugin.");
+        if (version_compare($local_version, $remote_version, '<')) {
+            $plugin->getLogger()->warning("Há uma nova versão disponível: $remote_version Baixe-a em: $remote_link");
+        }
     }
-} else {
-    $server->getLogger()->warning("Não foi possível baixar o arquivo de versão do plugin.");
-}
